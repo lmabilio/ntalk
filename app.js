34f3d46e816ef460b7f43e57a79446f60c657af2
lmabilio <lmabilio@gmail.com>
2013-12-03 18:31:44 -0200

    Commit node

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+	, app = express()
+	, load = require('express-load')
+	, error = require('./middleware/error')
+	, server = require('http').createServer(app)
+	, io = require('socket.io').listen(server);
+
+// all environments
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.use(error.notFound);
+app.use(error.serverError);
+
+//app.get('/', routes.index);
+//app.get('/usuarios', routes.user.index);
+
+load('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
+
+io.sockets.on('connection', function (client) {
+	client.on('send-server', function (data) {
+		var msg = "<b>"+data.nome+":</b> "+data.msg+"<br>";
+		client.emit('send-client', msg);
+		client.broadcast.emit('send-client', msg);
+	});
+});
+
+app.listen(3000, function(){
+	console.log("Ntalk no ar.");
+});
---
+++ models/usuario.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+
+  var contato = {
+  	nome: String,
+  	email: String
+  };
+
+  var usuario = {
+  	nome: String,
+  	email: String,
+  	contatos: contato
+  };
+ 
+};
